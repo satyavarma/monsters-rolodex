d72bc85ae4e97855e2d27c1d3e4c309ea32cdee5
satyavarma <rudrarajusatyavarma@gmail.com>
2020-06-07 16:58:41 +0530

    Updates

---
+++ precache-manifest.2f6e4cedbc09ba932467a201c61bf16d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71333ea06e291b01835c78d5d17dbca8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4d087c1c5116f6439fe0",
+    "url": "/monsters-rolodex/static/css/main.71e195ba.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d087c1c5116f6439fe0",
+    "url": "/monsters-rolodex/static/js/main.e312c433.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
